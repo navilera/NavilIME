9a11d066c7675f02b19d4a0a794817da7b65bbc0
Manwoo Yi <yimanwoo@gmail.com>
2019-01-19 16:34:44 -0800

    Initial filis

---
+++ debug.cmd
@@ -0,0 +1,4 @@
+del navilime.log
+copy /y NavilIME\Keyboard.dat "C:\Program Files\NavilIME"
+copy /y x64\Debug\NavilIME.dll "C:\Program Files\NavilIME"
+regsvr32 "c:\Program Files\NavilIME\NavilIME.dll"
---
+++ install.cmd
@@ -0,0 +1,3 @@
+copy /y NavilIME\Keyboard.dat "C:\Program Files\NavilIME"
+copy /y x64\Release\NavilIME.dll "C:\Program Files\NavilIME"
+regsvr32 "c:\Program Files\NavilIME\NavilIME.dll"
