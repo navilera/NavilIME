7dde5151ac0e6982dfee1a6c674a31c94166d157
Manwoo Yi <navilera@gmail.com>
2019-04-27 17:38:32 -0700

    [#8] Add Control panel including Keyboard layout selector

---
+++ navilIME_control/navilIME_control/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace navilIME_control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage_Keyboard = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button_Cancel = new System.Windows.Forms.Button();
+            this.button_Ok = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.comboBox_keyboards = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage_Keyboard.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage_Keyboard);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(732, 546);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage_Keyboard
+            // 
+            this.tabPage_Keyboard.Controls.Add(this.groupBox1);
+            this.tabPage_Keyboard.Controls.Add(this.panel1);
+            this.tabPage_Keyboard.Location = new System.Drawing.Point(8, 39);
+            this.tabPage_Keyboard.Name = "tabPage_Keyboard";
+            this.tabPage_Keyboard.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_Keyboard.Size = new System.Drawing.Size(716, 499);
+            this.tabPage_Keyboard.TabIndex = 0;
+            this.tabPage_Keyboard.Text = "Keyboards";
+            this.tabPage_Keyboard.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button_Ok);
+            this.panel1.Controls.Add(this.button_Cancel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(3, 408);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 88);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.panel5);
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(710, 405);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Keyboards";
+            // 
+            // button_Cancel
+            // 
+            this.button_Cancel.Location = new System.Drawing.Point(31, 13);
+            this.button_Cancel.Name = "button_Cancel";
+            this.button_Cancel.Size = new System.Drawing.Size(165, 62);
+            this.button_Cancel.TabIndex = 0;
+            this.button_Cancel.Text = "Cancel";
+            this.button_Cancel.UseVisualStyleBackColor = true;
+            this.button_Cancel.Click += new System.EventHandler(this.button_Cancel_Click);
+            // 
+            // button_Ok
+            // 
+            this.button_Ok.Location = new System.Drawing.Point(219, 13);
+            this.button_Ok.Name = "button_Ok";
+            this.button_Ok.Size = new System.Drawing.Size(165, 62);
+            this.button_Ok.TabIndex = 1;
+            this.button_Ok.Text = "OK";
+            this.button_Ok.UseVisualStyleBackColor = true;
+            this.button_Ok.Click += new System.EventHandler(this.button_Ok_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(3, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(99, 375);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(605, 27);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(102, 375);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Transparent;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(102, 27);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(503, 100);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Transparent;
+            this.panel5.Controls.Add(this.comboBox_keyboards);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(102, 127);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(503, 275);
+            this.panel5.TabIndex = 3;
+            // 
+            // comboBox_keyboards
+            // 
+            this.comboBox_keyboards.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox_keyboards.FormattingEnabled = true;
+            this.comboBox_keyboards.Items.AddRange(new object[] {
+            "세벌식 3-18Na",
+            "세벌식 3-90",
+            "세벌식 3-91 (최종)",
+            "두벌식",
+            "두벌식 옛한글",
+            "세벌식 두벌 레이아웃",
+            "세벌식 no shift",
+            "세벌식 옛한글",
+            "로마자"});
+            this.comboBox_keyboards.Location = new System.Drawing.Point(0, 0);
+            this.comboBox_keyboards.Name = "comboBox_keyboards";
+            this.comboBox_keyboards.Size = new System.Drawing.Size(503, 33);
+            this.comboBox_keyboards.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 546);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NavilIME Control Panel";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage_Keyboard.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage_Keyboard;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button_Ok;
+        private System.Windows.Forms.Button button_Cancel;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox comboBox_keyboards;
+    }
+}
+
---
+++ navilIME_control/navilIME_control/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace navilIME_control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (IsUserAdministrator() == false)
+            {
+                MessageBox.Show("Please run this program as Administrator");
+                Close();
+            }
+
+            Load_keyboard_combo();
+        }
+
+        private bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+        private void button_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button_Ok_Click(object sender, EventArgs e)
+        {
+            Store_keyboard_layout();
+        }
+
+        private string keyboardFileName = Environment.ExpandEnvironmentVariables("%ProgramW6432%") + "\\NavilIME\\Keyboard.dat";
+        private string keyboardFileName_x86 = Environment.ExpandEnvironmentVariables("%ProgramFiles(x86)%") + "\\NavilIME\\Keyboard.dat";
+
+        /* libhangul 라이브러리에서 바로 읽어 오는 것이 가장 좋으나 귀찮아서 그냥 하드 코딩.
+         * 어차피 자주 바뀌는 데이터도 아님.
+         * 윈폼 디자인에 comboBox_keyboards 컴포넌트의 아이템 순서와 키보드 레이아웃 아이디 순서가 일치해야함.
+         * 수정하기 전에 윈폼 디자인 설정을 먼저 확인할 것.
+         */
+        private string[] layoutName = { "318Na", "39", "3f", "2", "2y", "32", "3s", "3y", "ro" };
+        private int initialKeyboardComboIndex = 0;
+
+        private void Load_keyboard_combo()
+        {
+            Debug.WriteLine("Keyboard file : " + keyboardFileName);
+            Debug.WriteLine("Keyboard file (x86) : " + keyboardFileName_x86);
+
+            string currentKeyboardLayout = System.IO.File.ReadAllText(keyboardFileName);
+            currentKeyboardLayout = currentKeyboardLayout.TrimEnd();
+            Debug.WriteLine("Current Keyboard layout ID: " + currentKeyboardLayout);
+
+            bool isMatch = false;
+            for(int i = 0; i < layoutName.Length; i++)
+            {
+                Debug.WriteLine("[" + layoutName[i] + "] == [" + currentKeyboardLayout + "]");
+                if (layoutName[i] == currentKeyboardLayout)
+                {
+                    Debug.WriteLine("Match");
+                    
+                    comboBox_keyboards.SelectedIndex = i;
+                    isMatch = true;
+                    break;
+                }
+            }
+
+            if (isMatch == false)
+            {
+                comboBox_keyboards.SelectedIndex = 0;
+            }
+
+            initialKeyboardComboIndex = comboBox_keyboards.SelectedIndex;
+        }
+
+        private void Store_keyboard_layout()
+        {
+            int selectedIndex = comboBox_keyboards.SelectedIndex;
+
+            if (initialKeyboardComboIndex == selectedIndex)
+            {
+                Debug.WriteLine("No changes");
+                return;
+            }
+
+            Debug.WriteLine("Selected Keyboard Layout : " + layoutName[selectedIndex]);
+
+            System.IO.File.WriteAllText(keyboardFileName, layoutName[selectedIndex]);
+            System.IO.File.WriteAllText(keyboardFileName_x86, layoutName[selectedIndex]);
+
+            initialKeyboardComboIndex = selectedIndex;
+
+            MessageBox.Show("자판 설정을 " + comboBox_keyboards.Items[selectedIndex] + "로 변경했습니다.");
+        }
+    }
+}
